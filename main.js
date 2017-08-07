2e8dc42a26823997dc2a61849a60f4485925fe70
clarkgh <demolisho@hotmail.com>
2017-08-07 10:30:02 -0700

    basic logic is up, taking a break

--- main.js
+++ main.js
@@ -18,5 +18,36 @@ function gameEnd() {
 }
 
 function animateSquare() {
+  const $square = $( '#square' );
+  var oldPosition = $square.offset(),
+    newPosition = moveSquare( $( '#container' ) ),
+    movementSpeed = calculateSpeed( [oldPosition.top, oldPosition.left], newPosition);
 
+    $square.animate({
+      left: newPosition[0],
+      top: newPosition[1]
+    },
+    movementSpeed,
+    function() {
+      animateSquare();
+    })
+}
+
+function moveSquare( $container ) {
+  var containerWidth = ( $container.width() - 50 ),
+    containerHeight = ( $container.height() - 50 ),
+    newWidth = Math.floor( Math.random() * containerWidth ),
+    newHeight = Math.floor( Math.random() * containerHeight);
+
+    return [newWidth, newHeight];
+}
+
+function calculateSpeed ( previousPosition, nextPosition ) {
+  var xCoordinate = Math.abs( previousPosition[0] - nextPosition[0] ),
+    yCoordinate = Math.abs( previousPosition[1] - nextPosition[1] ),
+    largerValue = xCoordinate > yCoordinate ? xCoordinate : yCoordinate,
+    speedModifier = 0.2,
+    speed = Math.ceil( largerValue/speedModifier );
+
+    return speed;
 }
